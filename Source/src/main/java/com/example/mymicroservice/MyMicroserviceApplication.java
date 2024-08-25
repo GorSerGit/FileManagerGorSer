94f591a4626315956045b2568e3085fdb15fa16e
GorSerGit <96002214+GorSerGit@users.noreply.github.com>
2024-08-25 20:37:57 +0400

    Add files via upload

---
+++ Source/src/main/java/com/example/mymicroservice/MyMicroserviceApplication.java
@@ -0,0 +1,11 @@
+package com.example.mymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyMicroserviceApplication.class, args);
+    }
+}
